631fde8fbcae8814806c6ad87ba2f89a7a357a8d
WB <rcchapuk@gmail.com>
2014-04-13 15:33:29 +0100

    Add comments

--- diversity/drv_gpio.h
+++ diversity/drv_gpio.h
@@ -3,8 +3,8 @@
 #ifndef DRV_GPIO_H_
 #define DRV_GPIO_H_
 
-void initGPIO(void);
-void updateVideoOutputSelectionOnGPIO(uint8_t strongestChannel);
+void initGPIO(void); // Sets up the GPIO for the video multiplexer
+void updateVideoOutputSelectionOnGPIO(uint8_t strongestChannel); // Updates PD2 and PD3 depending on which RSSI channel has the strongest signal
 
 
 
